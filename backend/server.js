3a2fb0209ec2dcc182a9f806c70359f5fa5f981d
abdulxaqdev <abdulkhak8tursunov@gmail.com>
2022-09-25 23:47:22 +0500

    deploy heroku

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require("express");
 const colors = require("colors");
 const cors = require('cors') 
@@ -19,6 +20,14 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/user", require("./routes/userRoutes"));
 app.use("/api/auth", require("./routes/authRoutes"));
+
+// Serve frontend
+if (process.env.NODE_ENV === "production") {
+ app.use(express.static(path.join(__dirname, "../frontend/build")))
+ app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
+} else { 
+ app.get('/', (req, res) => res.send("Please set to production"))
+}
 
 app.use(errorHandler)
 
